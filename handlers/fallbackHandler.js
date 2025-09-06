3df2c1621415da5e93000d1af7a2de58a779dd3f
lunamediano <post@avifi.no>
2025-09-06 09:57:13 +0200

    Delete handlers directory

--- handlers/fallbackHandler.js
+++
@@ -1,8 +0,0 @@
-async function handle() {
-  return {
-    type: 'answer',
-    text: 'Jeg er ikke helt sikker – men jeg kan sjekke kunnskapsbasen nærmere om du spesifiserer hva du lurer på.'
-  };
-}
-
-module.exports = { handle };
