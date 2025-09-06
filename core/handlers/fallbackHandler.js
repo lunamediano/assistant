bd7dbc2a5958e3455732d6f60b8e3dcc5052a249
lunamediano <post@avifi.no>
2025-09-06 09:53:54 +0200

    Create fallbackHandler.js

---
+++ core/handlers/fallbackHandler.js
@@ -0,0 +1,8 @@
+async function handle() {
+  return {
+    type: 'answer',
+    text: 'Jeg er ikke helt sikker – men jeg kan sjekke kunnskapsbasen nærmere om du spesifiserer hva du lurer på.'
+  };
+}
+
+module.exports = { handle };
