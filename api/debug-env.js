17b625627fc5fa70621312a6ed10ebb203f13849
lunamediano <post@avifi.no>
2025-09-07 08:45:34 +0200

    Delete api/debug-env.js

--- api/debug-env.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.json({
-    has_OPENAI_API_KEY: !!process.env.OPENAI_API_KEY,
-    // legg gjerne til andre du vil sjekke:
-    // has_OPENAI_KEY: !!process.env.OPENAI_KEY,
-    // demo: process.env.DEMO || null,
-    runtime: process.env.VERCEL ? "vercel" : "local"
-  });
-}
